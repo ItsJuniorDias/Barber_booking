5490c1484227c8036217db552fd4c2dae887c2de
Alexandre <reabilitado97@gmail.com>
2025-08-27 16:32:30 -0300

    chore: defautl project

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,13 @@
+import { Image } from "expo-image";
+import { View, StyleSheet } from "react-native";
+
+export default function TabTwoScreen() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+});
